d8893b4108ac8732aeb7334fbcae802de4157a56
RoadRageFrance <38570453+RoadRageFrance@users.noreply.github.com>
2018-04-26 00:04:49 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,16 +1,29 @@
 const Discord = require('discord.js');
+const bot = new Discord.Client()
 
-var bot = new Discord.Client();
+var prefix = ("t!");
 
 bot.on('ready', () => {
-    console.log("Bot Ready !");
+    console.log("Bot prêt !");
 })
 
-bot.login('NDM2OTM2Mzk3Nzc4MTkwMzY3.DbuwRA.8ZnR9OULusJEKEKRyDUpTu7POfg')
+bot.login('NDM2OTM2Mzk3Nzc4MTkwMzY3.DcJJKg.XSJXWupexO9q31n5PYHCPP_sojk')
+
+bot.on("guildMemberAdd", member => {
+member.guild.channels.find("name", "accueil-messages").send('Bienvenue sur le serveur Road Rage France Communauté !')
+console.log("join")
+})
 
 bot.on('message', message => {
-if (message.content === "t!ping"){
-    message.reply("pong");
-    console.log('ping ping');
+
+if (message.content.startsWith( prefix + "help")) {
+    message.channel.send("__**Voici les commandes disponibles**__ :\n\n __**Légende**__ :\n\n ✅ : __Commande Disponible__\n ❌ : __Commande temporairement désactivée__\n 🚧 : __Commande en cours de développement__\n\n __t!help__ Pour la liste des commandes ✅\n __t!chaine__ pour avoir le lien de ma chaîne YouTube ✅");
+    console.log("help");
 }
+
+if (message.content.startsWith(prefix + "chaine")) {
+    message.channel.send("Ma chaîne YouTube ! https://www.youtube.com/c/RoadRageFrance13");
+    console.log("chaine")
+}
+
 });
